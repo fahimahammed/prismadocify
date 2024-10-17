893bd9c68fa9522c44862fd8d0b7a192f2b77767
Daniel Tayal <tayaldaniel2@gmail.com>
2024-10-17 18:34:48 +0530

    Update templateLoader.ts

--- src/templateLoader.ts
+++ src/templateLoader.ts
@@ -1,39 +1,35 @@
 import { promises as fs } from 'fs';
 import path from 'path';
+import Handlebars from 'handlebars';
 
 const defaultTemplate = `
 # Prisma Schema Documentation
-
 {{#each models}}
 ## {{this.name}}
-
 | Field | Type | List | Required | Unique |
 |-------|------|------|----------|--------|
 {{#each this.fields}}
 | {{name}} | {{type}} | {{#if isList}}Yes{{else}}No{{/if}} | {{#if isRequired}}Yes{{else}}No{{/if}} | {{#if isUnique}}Yes{{else}}No{{/if}} |
 {{/each}}
-
 {{/each}}
-
 ## Enums
-
 {{#each enums}}
 ### {{this.name}}
-
 {{this.values.join(", ")}}
 {{/each}}
 `;
 
-const loadTemplate = async (templatePath: string): Promise<string> => {
+const loadTemplate = async (templatePath: string): Promise<Handlebars.TemplateDelegate> => {
     try {
         if (templatePath) {
-            return await fs.readFile(templatePath, 'utf-8');
+            const templateContent = await fs.readFile(templatePath, 'utf-8');
+            return Handlebars.compile(templateContent);
         } else {
-            return defaultTemplate;
+            return Handlebars.compile(defaultTemplate);
         }
     } catch (error) {
         console.error('Error loading template, using default:', error);
-        return defaultTemplate;
+        return Handlebars.compile(defaultTemplate);
     }
 };
 
